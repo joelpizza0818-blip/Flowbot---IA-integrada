97b459c40cd1e5bf42360a4466336d42260bb0a7
Joel Alberto <joelpizza0818@gmail.com>
2026-04-03 15:51:39 -0400

    exposes API

--- Flowbot/src/chatbotLogic.js
+++ Flowbot/src/chatbotLogic.js
@@ -1,6 +1,6 @@
 // chatbotLogic.js
 
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY || 'AIzaSyAcg7HlxMx3wzGP39RtIW630yzySqsR43E';
 
 const MODELS = [
   "gemini-2.5-flash",
