51609ca097a1aedc640c09d376670909649cfd25
Joel Alberto <joelpizza0818@gmail.com>
2026-04-03 17:22:11 -0400

    fix: base path for github pages

--- Flowbot/vite.config.js
+++ Flowbot/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Flowbot---IA-integrada/'
-
+  base: '/Flowbot---IA-integrada/' // 👈 CLAVE
 })
